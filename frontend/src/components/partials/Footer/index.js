bf1a66d56b77efa6e919fa2aa2f9f480ea509855
gbcbaptista <66979159+gbcbaptista@users.noreply.github.com>
2021-04-09 00:36:27 -0300

    Building Template
    Creating the Header and Footer as components and used it inside the Template hugging the Route component.

---
+++ frontend/src/components/partials/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FooterArea} from './styled';
+
+const Header = () => {
+    return (
+        <FooterArea>
+            ...
+        </FooterArea>
+    );
+}
+
+export default Header;
---
+++ frontend/src/components/partials/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { HeaderArea} from './styled';
+
+const Header = () => {
+    return (
+        <HeaderArea>
+            ...
+        </HeaderArea>
+    );
+}
+
+export default Header;
